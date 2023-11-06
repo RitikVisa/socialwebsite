3b83fccaf506a427b0b6ae03f4ddb3041ae115e8
Ritik Visapurkar <121254833+RitikVisa@users.noreply.github.com>
2023-11-06 13:56:15 +0530

    transitions and overflow hidden

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-const observer = new IntersectionObserver((entries) =>{
+// const observer = new IntersectionObserver((entries) =>{
+//     entries.forEach((entry) => {
+//         console.log(entry);
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show');
+//         }else{
+//             entry.target.classList.remove('show');
+//         }
+//     })
+// })
+
+// const hiddenElement = document.querySelectorAll('.hidden');
+// hiddenElement.forEach((el) => observer.observe(el));
+
+// const hiddenElement2 = document.querySelectorAll('.hidden-2');
+// hiddenElement2.forEach((el) => observer.observe(el));
+const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry);
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        }else{
+        } else {
             entry.target.classList.remove('show');
         }
-    })
-})
+    });
+}, {
+    threshold: 0.2, // Adjust this threshold value as needed
+});
 
 const hiddenElement = document.querySelectorAll('.hidden');
 hiddenElement.forEach((el) => observer.observe(el));
